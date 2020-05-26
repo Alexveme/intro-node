b7f242e98eab307c72480f2cb03b5c5364c849ae
Alexveme <62341832+Alexveme@users.noreply.github.com>
2020-05-26 12:00:38 +0200

    sum added

--- index.js
+++ index.js
@@ -30,9 +30,20 @@ export default class SortedList {
         }
       
         min() {
+            if (this.items.length == 0) {
+                throw new Error('EmptySortedList');
+            } else {
+                return this.items[0];
+            };
         }
       
-        sum() {}
+        sum() {
+            let suma = 0;
+            this.items.forEach(item => {
+                suma += item;
+            });
+            return suma
+        }
       
         avg() {}
     }

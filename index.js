5e17864553eb7686a4be6cd422a491eb1e349749
Alexveme <62341832+Alexveme@users.noreply.github.com>
2020-05-26 11:39:27 +0200

    min test sorted

--- index.js
+++ index.js
@@ -21,9 +21,16 @@ export default class SortedList {
             };
         }
       
-        max() {}
+        max() {
+            if (this.items.length == 0) {
+                throw new Error('EmptySortedList');
+            } else {
+                return this.items[this.length -1];
+            };
+        }
       
-        min() {}
+        min() {
+        }
       
         sum() {}
       
